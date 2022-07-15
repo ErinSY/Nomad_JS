cff72dd8e07203c44b47b995e3375cb44afa7446
하서율 <seoyul0203@gmail.com>
2022-07-15 14:27:25 +0900

    버튼을눌러 추가된 리스트 삭제하기

--- prac/todolist/main.js
+++ prac/todolist/main.js
@@ -1,21 +1,30 @@
-const form = document.querySelector('form')
-const inputbox = document.querySelector('input')
-const list = document.querySelector('ul')
+const form = document.querySelector("form");    //form tag
+const inputbox = document.querySelector("#todo--iputBox");   // input tag
+const list = document.querySelector("ul");     //ul tag
+ 
 
-function listItems (event){
-  event.preventDefault();
-  const newList = inputbox.value;   // 입력값 변수에 담아주기
-  inputbox.value = '' ;              // 입력칸 비우기
-  addlist(newList)
+function deleteitems(event) {
+  console.dir(event);
+  const li = event.target.parentElement;    //  event > path > target => 타겟요소를 정할 수 있다.
+  li.remove();
 }
 
-function addlist (newList) {
-  const li = document.createElement('li');
-  const span = document.createElement('span');
-  list.appendChild(li)
-  li.appendChild(span);
+function addlist(newList) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
   span.innerText = newList;
-
+  const button = document.createElement("button");
+  button.innerText = " X ";
+  button.addEventListener("click", deleteitems);
+  li.appendChild(span);
+  li.appendChild(button);
+  list.appendChild(li);
 }
 
-form.addEventListener('submit',listItems)
+function listItems(event) {
+  event.preventDefault();
+  const newList = inputbox.value; // 입력값 변수에 담아주기
+  inputbox.value = ""; // 입력칸 비우기
+  addlist(newList);
+}
+form.addEventListener("submit", listItems);
